Fix #1 router token embedding ft gradients + add test

src/levanter/main/rlora_train.py
@@ -64,16 +64,15 @@ class TrainLmConfig:
             raise ValueError("Can't have embedding_router_token_ft without add_router_token")
 
 
-def filter_embedding_grads(optimizer: optax.GradientTransformation, Embed, Vocab, token_mask: hax.NamedArray):
+def filter_embedding_grads(Embed, Vocab, token_mask: hax.NamedArray):
     def where(m: RQwenLMHeadModel):
-        return [m.embeddings]
+        return [m.embeddings.token_embeddings]
 
     def replace_fn(x):
-        if isinstance(x, hnn.Embedding):
-            assert x.weight is not None, "No embedding updates, is embedding_ft True?"
-            new_grads = x.weight * token_mask.broadcast_to((Vocab, Embed))
-            return dataclasses.replace(x, weight=new_grads)
-        return x
+        assert isinstance(x, hnn.Embedding), f"Expected Embedding, got {type(x)}"
+        assert x.weight is not None, "No embedding updates, is embedding_ft True?"
+        new_grads = x.weight * token_mask.broadcast_to((Vocab, Embed))
+        return dataclasses.replace(x, weight=new_grads)
 
     def update_fn(updates, state, params=None):
         del params
@@ -82,7 +81,7 @@ def filter_embedding_grads(optimizer: optax.GradientTransformation, Embed, Vocab
 
     mask_transform = optax.GradientTransformation(lambda _: optax.EmptyState(), update_fn)
 
-    return optax.chain(optimizer, mask_transform)
+    return mask_transform
 
 
 def compute_next_token_loss(
@@ -210,7 +209,7 @@ def main(config: TrainLmConfig):
         token_mask = hax.nn.one_hot(
             tokenizer.convert_tokens_to_ids(config.data.router_token), Vocab, dtype=jnp.float32
         )
-        optimizer = filter_embedding_grads(optimizer, config.model.Embed, Vocab, token_mask)
+        optimizer = optax.chain(optimizer, filter_embedding_grads(config.model.Embed, Vocab, token_mask))
 
     # some axes we need
     Batch = config.trainer.TrainBatch

tests/test_rlora_train.py
@@ -1,26 +1,26 @@
 import os
 import tempfile
 
+import jax
+import jax.numpy as jnp
 import jmp
 import pytest
+
+import haliax as hax
 
 import levanter.main.rlora_train as rlora_train
 import tiny_test_corpus
 from levanter.data.text import FIMUrlSourceConfig
 from levanter.distributed import RayConfig
 from levanter.models.rotary import DefaultRotaryEmbeddingsConfig
+from levanter.models.routed_lora_model import RQwenLMHeadModel
 from levanter.optim.config import AdamConfig
 from levanter.tracker.wandb import WandbConfig
 from test_utils import skip_if_no_torch
 
 
-@pytest.mark.entry
-@skip_if_no_torch
-def test_rlora_train():
-    from transformers import Qwen2ForCausalLM
-
-    # just testing if train_lm has a pulse
-    model_cfg = rlora_train.RQwenConfig(
+def small_model_cfg():
+    return rlora_train.RQwenConfig(
         num_layers=4,
         num_heads=2,
         num_kv_heads=2,
@@ -36,6 +36,15 @@ def test_rlora_train():
         use_layer_norm_weight=True,
         rope=DefaultRotaryEmbeddingsConfig(theta=1000000.0),
     )
+
+
+@pytest.mark.entry
+@skip_if_no_torch
+def test_rlora_train():
+    from transformers import Qwen2ForCausalLM
+
+    # just testing if train_lm has a pulse
+    model_cfg = small_model_cfg()
     with tempfile.TemporaryDirectory() as tmpdir:
         test_data_jsonl = tiny_test_corpus.write_fim_data(tmpdir + "/test_data.jsonl", len=2048)
         test_validation_jsonl = tiny_test_corpus.write_fim_data(tmpdir + "/test_data_valid.jsonl", len=2048)
@@ -84,3 +93,32 @@ def test_rlora_train():
                 os.unlink("wandb")
             except Exception:
                 pass
+
+
+def test_embedding_grad_filter():
+    from levanter.main.rlora_train import filter_embedding_grads
+
+    # test that the filter works
+    model_cfg = small_model_cfg()
+    Vocab = hax.Axis("vocab", 100)
+    model = RQwenLMHeadModel.init(Vocab, model_cfg, key=jax.random.PRNGKey(0))
+
+    def replace_with_ones(x):
+        return hax.ones_like(x)
+
+    def is_leaf(x):
+        return isinstance(x, hax.NamedArray)
+
+    model: RQwenLMHeadModel = jax.tree.map(replace_with_ones, model, is_leaf=is_leaf)
+    assert model.embeddings.token_embeddings.weight.array[0, 0].item() == 1.0
+
+    token_mask = hax.ones(Vocab, dtype=jnp.bool).at[Vocab, 50].set(0.0)
+    tform = filter_embedding_grads(model.Embed, Vocab, token_mask)
+    res, _ = tform.update(model, ())
+    res: RQwenLMHeadModel
+    token_embs = res.embeddings.token_embeddings.weight
+    assert hax.all(
+        token_embs[Vocab, 50] == 0.0
+    ).item(), f"Token 50 should be zeroed out, got {token_embs[Vocab, 50].tolist()}"
+
+    print(model)